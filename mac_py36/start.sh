734b7f74b7fcb519b79d56860f03677db383add5
Divano <zhengtianyu@baidu.com>
2021-02-18 16:05:38 +0800

    Update start.sh

--- mac_py36/start.sh
+++ mac_py36/start.sh
@@ -1,6 +1,8 @@
 export PATH=/Library/Frameworks/Python.framework/Versions/3.6/bin:$PATH
 
 export FLAGS_call_stack_level=2
+export PYTHONPATH=`pwd`:$PYTHONPATH
+cd task
 # cases=`find /Users/paddle/zty/baidu/paddle/test/nn-test -name "test*.py" | sort`
 # ignore="test_randint.py \
 # test_randperm.py \
