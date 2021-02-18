e30ff0c3a8fc78444c0b335cd7273dd219992ad2
DDDivano <zhengtianyu@baidu.com>
2021-02-18 14:46:50 +0800

    add mac py36

---
+++ mac_py36/start.sh
@@ -0,0 +1,26 @@
+export PATH=/Library/Frameworks/Python.framework/Versions/3.6/bin:$PATH
+
+export FLAGS_call_stack_level=2
+# cases=`find /Users/paddle/zty/baidu/paddle/test/nn-test -name "test*.py" | sort`
+# ignore="test_randint.py \
+# test_randperm.py \
+# test_lstm_cudnn.py \
+# test_conv1d.py \
+# test_truncatedNormalInitializer.py \
+# "
+# for file in ${cases}
+# do
+#     echo ${file}
+#     if [[ ${ignore} =~ ${file##*/} ]]; then
+
+#         echo "跳过"
+
+#     else
+
+#         pytest -sv ${file} --junitxml=result/${file##*/}_log.xml
+
+#     fi
+    
+# done
+
+pytest -sv nn-test/test_abs.py
